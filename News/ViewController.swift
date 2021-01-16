606bf2ed048ca5e4eaf1b9c34762aa7522eb17a0
Giovanni Bruno <giovannibruno128@gmail.com>
2021-01-16 20:05:05 -0300

    Initial commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Giovanni Bruno on 16/01/21.
+//  Copyright © 2021 Pomappo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
